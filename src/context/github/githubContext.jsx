66f122c84b177661b1f8686567612f404a6b16af
aamir-ullah-offical <aamirullah.edu.pk@gmail.com>
2025-10-04 09:42:12 +0500

    GitHub finder 2.0.1 in React-Vite

--- src/context/github/githubContext.jsx
+++ src/context/github/githubContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useContext } from "react";
 
 const GithubContext = createContext();
 
-// ✅ custom hook for cleaner usage
 export const useGithub = () => {
   return useContext(GithubContext);
 };
